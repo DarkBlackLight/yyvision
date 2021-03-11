f0d348fc54061d468669139b494923f2792ae034
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-03-11 15:55:16 +0800

    minor fix

--- app/controllers/concerns/admin_engines_concern.rb
+++ app/controllers/concerns/admin_engines_concern.rb
@@ -1,12 +1,9 @@
 module AdminEnginesConcern
   extend ActiveSupport::Concern
+  require "base64"
+
   included do
     private
-
-    def index
-      require "base64"
-      super
-    end
 
     def resource_params
       params.require(:engine).permit(:full_name, :address, :workers, :engine_type, :device)
