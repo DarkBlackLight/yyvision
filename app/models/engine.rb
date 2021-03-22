dfb46adebaf26f798bf5968d6b7a59dcae0dd96f
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-03-22 17:38:37 +0800

    minor fix

--- app/models/engine.rb
+++ app/models/engine.rb
@@ -20,7 +20,7 @@ class Engine < ApplicationRecord
     unless self.secret
       o = [(0..9), ('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
       self.secret = (loop do
-        token = (0...20).map { o[rand(o.length)] }.join
+        token = (0...10).map { o[rand(o.length)] }.join
         break token unless Engine.where(secret: token).exists?
       end)
     end
