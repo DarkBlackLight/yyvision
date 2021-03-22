bed56b7504434db98076b3702bcf9f4cd064d58a
FlavorBones <512354665@qq.com>
2021-03-22 19:01:35 +0800

    small change

--- app/controllers/admin/cameras_controller.rb
+++ app/controllers/admin/cameras_controller.rb
@@ -1,4 +1,9 @@
 class Admin::CamerasController < Admin::ResourcesController
   include AdminCamerasConcern
+  
+  def filter_params
+    params.slice(:query_name, :query_status, :query_location_id)
+  end
+
 end
 
