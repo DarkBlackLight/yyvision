2213a63d9ab0a288bedc72636b86a232ecf99194
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-03-26 11:20:58 +0800

    minor fix

--- db/migrate/20210326031929_add_problem_details.rb
+++ db/migrate/20210326031929_add_problem_details.rb
@@ -1,6 +1,6 @@
 class AddProblemDetails < ActiveRecord::Migration[6.1]
   def change
     add_column :problems, :problem_status, :integer, default: 0
-    add_column :problems, :location_id, :integer, default: 0
+    add_column :problems, :location_id, :integer
   end
 end
