13eedb5d22ce8bb0d553e62470d0584e8a7618e1
FlavorBones <512354665@qq.com>
2021-03-30 20:32:04 +0800

    small change

--- app/controllers/concerns/admin_location_events_concern.rb
+++ app/controllers/concerns/admin_location_events_concern.rb
@@ -2,10 +2,6 @@ module AdminLocationEventsConcern
   extend ActiveSupport::Concern
   included do
 
-    def stats_location_events_group_status
-      render json: {correct: LocationEvent.accessible_by(current_ability, :read).filterable(filter_params).where(active: true).size, error: LocationEvent.accessible_by(current_ability, :read).filterable(filter_params).where(active: false ).size}.as_json, status: :ok
-    end
-
     private
 
     def filter_params
