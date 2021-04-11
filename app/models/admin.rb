3c8947f1bdd8b7b55f5c39c92753ba64f7c46a14
FlavorBones <512354665@qq.com>
2021-04-11 11:36:29 +0800

    small cgange

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
   has_one :user, as: :source, dependent: :destroy
   accepts_nested_attributes_for :user
 
-  belongs_to :location
+  belongs_to :location, optional: false
 
   scope :query_name, -> (q) { where('lower(full_name) like lower(?)', "%#{q.downcase}%") }
   scope :query_role, ->(q) { where role: q }
