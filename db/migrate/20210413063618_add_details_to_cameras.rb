e9ebc2ac76e5c53263f9ed95e47856f6c56a6174
FlavorBones <512354665@qq.com>
2021-04-13 19:26:42 +0800

    small change

--- db/migrate/20210413063618_add_details_to_cameras.rb
+++ db/migrate/20210413063618_add_details_to_cameras.rb
@@ -1,5 +1,5 @@
 class AddDetailsToCameras < ActiveRecord::Migration[6.1]
   def change
-    add_column :cameras, :marked, :boolean
+    add_column :cameras, :marked, :boolean, default: false
   end
 end
