ae09600e5291bacf2b4aeed4989c1c5fb70a8694
FlavorBones <512354665@qq.com>
2021-04-13 17:54:30 +0800

    small change

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,7 @@ class Notification < ApplicationRecord
 
   belongs_to :admin
   has_many :notification_attachments, dependent: :destroy
+  accepts_nested_attributes_for :notification_attachments, allow_destroy: true
   has_many :notification_views, dependent: :destroy
 
 end
