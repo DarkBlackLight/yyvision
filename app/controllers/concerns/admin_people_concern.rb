7dc7a1f3da6beb7d242c03859047bdbfce1ab1b9
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-04-15 15:53:11 +0800

    minor fix

--- app/controllers/concerns/admin_people_concern.rb
+++ app/controllers/concerns/admin_people_concern.rb
@@ -4,7 +4,7 @@ module AdminPeopleConcern
     private
 
     def resource_params
-      params.require(:person).permit(:name, portraits_attributes: [:id, :index, :img, :_destroy], bank_ids: [])
+      params.require(:person).permit(:name, portraits_attributes: [:id, :index, :_destroy], bank_ids: [])
     end
   end
 end
