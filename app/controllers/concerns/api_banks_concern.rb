440c0d66fe1810379a9b031c3fc33c01dff39ddb
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-04-16 20:08:41 +0800

    minor fix

--- app/controllers/concerns/api_banks_concern.rb
+++ app/controllers/concerns/api_banks_concern.rb
@@ -4,11 +4,11 @@ module ApiBanksConcern
     private
 
     def set_show_json(resource)
-      resource.as_json(only: [:id, :index, :name, :parent_id])
+      resource.as_json(only: [:id, :index, :name, :ancestry])
     end
 
     def resource_params
-      params.require(:bank).permit(:index, :name, :parent_id)
+      params.require(:bank).permit(:index, :name, :ancestry)
     end
 
   end
--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,6 +1,5 @@
 class Bank < ApplicationRecord
-  belongs_to :parent, class_name: 'Bank', optional: true
-  has_many :children, class_name: 'Bank'
+  has_ancestry
 
   has_many :bank_people, dependent: :destroy
   has_many :people, through: :bank_people
