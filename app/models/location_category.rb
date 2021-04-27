d26092a0bd1d26794b737118051f0249bdbf1329
FlavorBones <512354665@qq.com>
2021-04-27 19:46:53 +0800

    small change

--- app/models/location_category.rb
+++ app/models/location_category.rb
@@ -3,7 +3,7 @@ class LocationCategory < ApplicationRecord
   has_many :locations
 
   scope :query_name, -> (q) { where('lower(name) like lower(?)', "%#{q}%") }
-  scope :query_level, ->(q) { where level: q }
+  scope :query_level, ->(q) { where index: q }
   scope :query_parent, ->(q) { where ancestry: q }
 
 end
