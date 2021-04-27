3cbe034e56689ecc20ff2728079770df0224b2e2
FlavorBones <512354665@qq.com>
2021-04-27 19:30:18 +0800

    small change

--- app/controllers/admin/problem_categories_controller.rb
+++ app/controllers/admin/problem_categories_controller.rb
@@ -1,9 +1,5 @@
 class Admin::ProblemCategoriesController < Admin::ResourcesController
   include AdminProblemCategoriesConcern
 
-  def filter_params
-    params.slice(:query_name,  :query_level, :query_parent)
-  end
-
 end
 
--- app/models/problem_category.rb
+++ app/models/problem_category.rb
@@ -2,7 +2,7 @@ class ProblemCategory < ApplicationRecord
   has_ancestry
   scope :query_name, -> (q) { where('lower(name) like lower(?)', "%#{q}%") }
   scope :query_level, ->(q) { where level: q }
-  scope :query_level, ->(q) { where ancestry: q }
+  scope :query_parent, ->(q) { where ancestry: q }
 
   enum level: [:common, :general, :serious]
 end
