48fa92b5573f811c4c02bea6114c9cde116a2d78
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-05-03 10:49:58 +0800

    minor fix

--- app/controllers/concerns/admin_events_concern.rb
+++ app/controllers/concerns/admin_events_concern.rb
@@ -4,7 +4,7 @@ module AdminEventsConcern
     private
 
     def resource_params
-      params.require(:event).permit(:if_holiday, :observation, :name, :nickname, :notify, :interval, :tolerance, :problem_tolerance, :problem_category_id, event_time_ranges_attributes: [:id, :start_time, :end_time, :_destroy])
+      params.require(:event).permit(:if_holiday, :observation, :name, :nickname, :notify, :enabled, :interval, :tolerance, :problem_tolerance, :problem_category_id, event_time_ranges_attributes: [:id, :start_time, :end_time, :_destroy])
     end
   end
 end
