51f2056204975059d7921c44a6efeb330456dee6
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-05-21 15:51:56 +0800

    minor fix

--- app/controllers/concerns/api_problems_concern.rb
+++ app/controllers/concerns/api_problems_concern.rb
@@ -4,11 +4,11 @@ module ApiProblemsConcern
     private
 
     def set_show_json(resource)
-      resource.as_json(only: [:id, :created_at])
+      resource.as_json(only: [:id, :issued_at, :created_at], include: { location: { include: { path: { only: [:id, :name] } } } })
     end
 
     def resource_params
-      params.require(:problem).permit(:location_id, :problem_category_id)
+      params.require(:problem).permit(:location_id, :status, :issued_at, :problem_category_id)
     end
 
   end
