8d49a4a46cd8d69e748944bcb9ee29f629ae27ab
FlavorBones <512354665@qq.com>
2021-05-23 17:26:36 +0800

    small change

--- app/controllers/concerns/api_problems_concern.rb
+++ app/controllers/concerns/api_problems_concern.rb
@@ -9,7 +9,6 @@ module ApiProblemsConcern
 
     def set_show_json(resource)
       resource.as_json(only: [:id, :problem_status, :discover_type, :note, :issued_at, :created_at],
-                       methods: [:img_data],
                        include: { location: { include: { path: { only: [:id, :name] } } },
                                   problem_category: { only: [:id, :name] },
                                   admin: { only: [:id, :full_name]},
