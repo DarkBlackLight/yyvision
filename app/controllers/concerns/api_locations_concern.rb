300c399995a4f255eb054d823d6941c780b31876
FlavorBones <512354665@qq.com>
2021-05-26 16:44:20 +0800

    small change

--- app/controllers/concerns/api_locations_concern.rb
+++ app/controllers/concerns/api_locations_concern.rb
@@ -4,18 +4,19 @@ module ApiLocationsConcern
     private
 
     def filter_params
-      params.slice(:query_physical)
-    end
-
-    def set_index_json(resources)
-      set_show_json(resources.includes([:event_locations, :cameras => :event_cameras]))
+      params.slice()
     end
 
     def set_show_json(resource)
-      resource.as_json(only: [:id, :name], methods: [:setting_event_ids],
-                       include: { cameras: { only: [:id, :rtsp, :status, :name, :enabled, :location_id, :confidence],
-                                             include: { event_cameras: { only: [:event_id, :box_a, :box_b, :box_c, :box_d, :line_a, :line_b] } } },
-                       })
+      resource.as_json(only: [:name, :id, :ancestry],
+                       include: { parent: { only: [:id, :name] },},
+                       include: { location_level: { only: [:id, :name, :index] },},
+                       )
     end
+
+    def resource_params
+      params.require(:location).permit(:lat, :lon, :name, :parent_id, :engine_id, :location_level_id, :location_category_id, setting_event_ids: [])
+    end
+
   end
 end
