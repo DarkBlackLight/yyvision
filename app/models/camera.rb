b1980c49e10beb9d9de660203318644d33096d01
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-05-28 10:22:12 +0800

    minor fix

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -13,8 +13,8 @@ class Camera < ApplicationRecord
   after_create :setup_location
   after_destroy :setup_location
 
-  validates :rtsp, uniqueness: true
-  validates :rtsp, presence: true
+  # validates :rtsp, uniqueness: true
+  # validates :rtsp, presence: true
 
   scope :query_name, -> (q) { where('lower(name) like lower(?)', "%#{q.downcase}%") }
   scope :query_status, ->(q) { where status: q }
@@ -22,11 +22,11 @@ class Camera < ApplicationRecord
   scope :query_location_id, -> (q) { joins(:location).where(:'location_id' => q) }
   scope :query_marked, ->(q) { where marked: q }
 
-  scope :query_location_id_0, -> (q) { where(location_id: Location.find(q).subtree_ids )}
-  scope :query_location_id_1, -> (q) { where(location_id: Location.find(q).subtree_ids )}
-  scope :query_location_id_2, -> (q) { where(location_id: Location.find(q).subtree_ids )}
-  scope :query_location_id_3, -> (q) { where(location_id: Location.find(q).subtree_ids )}
-  scope :query_location_id_4, -> (q) { where(location_id: Location.find(q).subtree_ids )}
+  scope :query_location_id_0, -> (q) { where(location_id: Location.find(q).subtree_ids) }
+  scope :query_location_id_1, -> (q) { where(location_id: Location.find(q).subtree_ids) }
+  scope :query_location_id_2, -> (q) { where(location_id: Location.find(q).subtree_ids) }
+  scope :query_location_id_3, -> (q) { where(location_id: Location.find(q).subtree_ids) }
+  scope :query_location_id_4, -> (q) { where(location_id: Location.find(q).subtree_ids) }
 
   def create_event_cameras
     location.event_locations.each do |event_location|
