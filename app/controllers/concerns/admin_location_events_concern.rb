651f00ab9d6f3f2d3285f4c20ed10f27e6496640
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-05-28 14:04:24 +0800

    minor fix

--- app/controllers/concerns/admin_location_events_concern.rb
+++ app/controllers/concerns/admin_location_events_concern.rb
@@ -4,6 +4,10 @@ module AdminLocationEventsConcern
 
     private
 
+    def index_order_by
+      'active_at desc'
+    end
+
     def filter_params
       params.slice(:query_active, :query_location_id_0, :query_location_id_1, :query_location_id_2, :query_location_id_3, :query_location_id_4, :query_event_id, :query_created_at_from, :query_created_at_to,)
     end
