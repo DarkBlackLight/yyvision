cfc4473f250cad62fc94fcb52071fc6d1e2111e6
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-05-28 13:34:12 +0800

    minor fix

--- app/controllers/concerns/api_locations_concern.rb
+++ app/controllers/concerns/api_locations_concern.rb
@@ -8,7 +8,7 @@ module ApiLocationsConcern
     end
 
     def set_index_json(resources)
-      set_show_json(resources.includes([:event_locations, :parent, :cameras => :event_cameras]))
+      set_show_json(resources.includes([:event_locations, :cameras => :event_cameras]))
     end
 
     def set_show_json(resource)
