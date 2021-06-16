c35d71250cf691e3528aba20c2bfaa4737c74e99
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-06-16 20:05:05 +0800

    minor fix

--- app/controllers/concerns/admin_people_concern.rb
+++ app/controllers/concerns/admin_people_concern.rb
@@ -7,6 +7,10 @@ module AdminPeopleConcern
       params.slice(:query_name)
     end
 
+    def create_success_path
+      url_for({ action: :edit, id: @resource.id })
+    end
+
     def resource_params
       params.require(:person).permit(:name, portraits_attributes: [:id, :index, :_destroy], bank_ids: [])
     end
