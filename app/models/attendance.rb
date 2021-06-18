24fb81f66b4cad5a00d0958c724533e26f8447b5
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-06-18 13:44:11 +0800

    minor fix

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,6 @@
 class Attendance < ApplicationRecord
   scope :query_person_id, -> (q) { where(person_id: q) }
-  scope :query_bank_id, -> (q) { joins(:person => :banks).where(banks: { id: q }) }
+  scope :query_bank_id, -> (q) { joins(:person => :banks).where(banks: { id: Bank.find(q).descendant_ids }) }
 
   belongs_to :person
   belongs_to :portrait
