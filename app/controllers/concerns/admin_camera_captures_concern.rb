d17274b89fe84e8a86436c24cbb627eea6f31dc0
FlavorBones <512354665@qq.com>
2021-08-16 18:27:26 +0800

    small change

--- app/controllers/concerns/admin_camera_captures_concern.rb
+++ app/controllers/concerns/admin_camera_captures_concern.rb
@@ -2,5 +2,10 @@ module AdminCameraCapturesConcern
   extend ActiveSupport::Concern
   included do
     private
+
+    def resource_params
+      params.require(:camera_capture).permit(:location_id, :camera_id, :engine_id, :img, :img_url, :bodies_count, :portraits_count)
+    end
+
   end
 end
