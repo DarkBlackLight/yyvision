b89e8f4eebe4a7695f951a3557fd0d79d1d7dc3d
FlavorBones <512354665@qq.com>
2021-09-06 14:21:30 +0800

    small change

--- app/controllers/concerns/admin_problems_concern.rb
+++ app/controllers/concerns/admin_problems_concern.rb
@@ -7,6 +7,9 @@ module AdminProblemsConcern
     def filter_params
       params.slice(:query_problem_status_arr, :query_discover_type, :query_location_id_0, :query_location_id_1, :query_location_id_2, :query_location_id_3, :query_location_id_4, :query_problem_status, :query_problem_category_id, :query_from_date, :query_to_date, :query_admin_id)
     end
+    def filter_report_params
+      params.slice(:query_problem_status_arr, :query_discover_type, :query_location_id_0, :query_location_id_1, :query_location_id_2, :query_location_id_3, :query_location_id_4, :query_problem_status, :query_problem_category_id, :query_admin_id)
+    end
 
     def resource_params
       params.require(:problem).permit(:reviewing_admin, :reviewing_at, :reviewing_note, :negated_admin, :negated_at, :negated_note, :corrected_admin, :corrected_at, :corrected_note, :problem_status, :discover_type, :problem_category_id, :issued_at, :note, :admin_id, :location_id,
