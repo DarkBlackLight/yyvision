142a0f5ae3ea2a534ff19daa1fe1d80df3eb3cd6
FlavorBones <512354665@qq.com>
2021-09-18 10:25:38 +0800

    small change

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,6 @@
 class Attendance < ApplicationRecord
   scope :query_person_id, -> (q) { where(person_id: q) }
-  scope :query_bank_id, -> (q) { joins(:person => :banks).where(banks: { id: Bank.find(q).descendant_ids }) }
+  scope :query_bank_id, -> (q) { joins(:person => :banks).where(banks: { id: Bank.find(q).descendants }) }
 
   belongs_to :person
   belongs_to :portrait
